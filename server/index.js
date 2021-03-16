33550f49b98e45f79fc0fa66237dd8c9d4348110
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2021-03-16 21:18:59 +0200

    completeProj

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(router);
+
+
+io.on('connect', (socket) => {
+console.log('new connection');
+
+socket.on('joinRoom', ({id, name}, callback) => {
+    const { error, user } = addUser({id: socket.id , name, id});
+    console.log(user)
+    if(error) return callback(error);
+
+    socket.join(user.id);
+
+    socket.emit('message', {user: 'admin', text: `${user.name} , welcome to the game`});
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined the room`});
+
+    io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom})
+
+    callback();
+});
+
+socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    
+    io.to(user.room).emit('message', {user: user.name, text: message});
+
+    callback;
+
+});
+
+socket.on('sendScenario', ({id, scenario}, callback) => {
+
+}
+
+
+socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user){
+        io.to(user.room).emit('message', {user:'admin', text:`${user.name} has left.`})
+    }
+    })
+});
+
+
+
+server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: false
+// }));
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.set('Content-Type', 'application/json');
+//   next();
+// });
