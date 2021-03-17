6c10eeb9289f6318cefbace97243918cb3ce8de6
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2021-03-18 00:20:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,11 +44,6 @@ socket.on('sendMessage', (message, callback) => {
 
 });
 
-socket.on('sendScenario', ({id, scenario}, callback) => {
-
-}
-
-
 socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
@@ -61,31 +56,3 @@ socket.on('disconnect', () => {
 
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({
-//     extended: false
-// }));
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.set('Content-Type', 'application/json');
-//   next();
-// });
